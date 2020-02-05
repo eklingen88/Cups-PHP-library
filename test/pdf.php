397d5d79087f52700bbb13cb7cf66fa8a00eb855
Isaac Marlon da Silva Lourenço <isaac.marlon@escolar.ifrn.edu.br>
2020-02-04 21:47:15 -0300

    Update pdf.php

--- test/pdf.php
+++ test/pdf.php
@@ -5,6 +5,8 @@ require_once "../Cups.php";
 $printer = new Cups\Printer;
 
 //$printer->submit( 'sample.pdf' );
-$printer->defaultSubmit( 'sample.pdf', 2);
+//$printer->defaultSubmit( 'sample.pdf', 2);
+//$printer->cupsRejectJobs( 'YourPrinter' );
+//$printer->cupsAcceptJobs( 'YourPrinter' );
 
 ?>
